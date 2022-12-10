04264671700972e457364fe8ed97c4c52c2aee73
atabekdemurtaza <mychannel21war@gmail.com>
2022-12-10 13:04:12 +0500

    10.12.2022

--- base/views.py
+++ base/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 from .products import products
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -17,13 +19,19 @@ def getRoutes(request):
 @api_view(['GET'])
 def getProducts(request):
     #return JsonResponse(products, safe=False)
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    """product = None
     for i in products:
         if i['_id'] == pk:
             product = i
-            break
-    return Response(product)
+            break"""
+    return Response(serializer.data)
